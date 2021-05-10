a16764a51e5317ca88c48df956f257adf7730d78
Joshua Hauhio <joshuahauhio@gmail.com>
2021-05-10 14:58:24 -0500

    random number app

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func randomInt(min int, max int) int {
+	rand := min + rand.Intn(max-min)
+
+	return rand
+}
+
+func randomString(len int) string {
+	letterByte := make([]byte, len)
+
+	for i := 0; i < len; i++ {
+		letterByte[i] = byte(randomInt(65, 90))
+	}
+
+	return string(letterByte)
+}
 
 func main() {
-	fmt.Println("hello world")
+	rand.Seed(time.Now().UnixNano())
+
+	fmt.Println(randomString(25))
 }
