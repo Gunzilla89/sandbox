ef39a35f38194692183d7f4169e84318042fd3d1
Joshua Hauhio <joshuahauhio@gmail.com>
2021-05-20 13:12:52 -0500

    added gorilla mux

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Gunzilla89/sandbox
 
 go 1.16
+
+require github.com/gorilla/mux v1.8.0 // indirect
--- main.go
+++ main.go
@@ -6,7 +6,16 @@ import (
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>Welcome</h1>")
+	w.Header().Set("Content-Type", "text/html")
+
+	if r.URL.Path == "/" {
+		fmt.Fprint(w, "<h1>Welcome</h1>")
+	} else if r.URL.Path == "/contact" {
+		fmt.Fprint(w, "To get in touch, please send an email to <a href=\"mailto:support@lenslocked.com\">support@lenslocked.com</a>")
+	} else {
+		w.WriteHeader((http.StatusNotFound))
+		fmt.Fprint(w, "<h1>We couldn't find the page :(</h1>")
+	}
 
 }
 func main() {
