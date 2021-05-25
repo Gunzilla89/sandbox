a5000047075d6d4c02ab6d11c89aeef73e14b946
Joshua Hauhio <joshuahauhio@gmail.com>
2021-05-25 08:34:02 -0500

    Created a views package, bootstrap template as well as a footer template

--- main.go
+++ main.go
@@ -4,35 +4,42 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/Gunzilla89/sandbox/views"
 	"github.com/gorilla/mux"
 )
 
+var homeView *views.View
+var contactView *views.View
+
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "<h1>Welcome</h1>")
+	if err := homeView.Template.ExecuteTemplate(w, homeView.Layout, nil); err != nil {
+		panic(err)
+	}
 }
 
 func contact(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "To get in touch, please send an email to <a href=\"mailto:support@lenslocked.com\">support@lenslocked.com</a>")
-}
-
-func faq(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "<h1>FAQ Page</h1>")
+	if err := contactView.Template.ExecuteTemplate(w, homeView.Layout, nil); err != nil {
+		panic(err)
+	}
 }
 
 func notFound(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	fmt.Fprint(w, "<h1>Oops!</h1>")
+
 }
 
 func main() {
+
+	homeView = views.NewView("bootstrap", "views/home.gohtml")
+	contactView = views.NewView("bootstrap", "views/contact.gohtml")
+
 	var notFoundHandle http.Handler = http.HandlerFunc(notFound)
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
-	r.HandleFunc("/faq", faq)
 	r.NotFoundHandler = notFoundHandle
 
 	http.ListenAndServe(":3000", r)
