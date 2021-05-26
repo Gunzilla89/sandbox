1d5c2b15125cf31e9b754f37679b9670665a56ae
Joshua Hauhio <joshuahauhio@gmail.com>
2021-05-26 15:54:41 -0500

    added sign up view and cleaned code

--- main.go
+++ main.go
@@ -10,36 +10,53 @@ import (
 
 var homeView *views.View
 var contactView *views.View
+var faqView *views.View
+var signupView *views.View
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	if err := homeView.Template.ExecuteTemplate(w, homeView.Layout, nil); err != nil {
-		panic(err)
-	}
+	checkAndRender(homeView.Render(w, nil))
 }
 
 func contact(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	if err := contactView.Template.ExecuteTemplate(w, homeView.Layout, nil); err != nil {
-		panic(err)
-	}
+	checkAndRender(contactView.Render(w, nil))
+}
+
+func faq(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	checkAndRender(faqView.Render(w, nil))
+}
+
+func signup(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	checkAndRender(signupView.Render(w, nil))
 }
 
 func notFound(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	fmt.Fprint(w, "<h1>Oops!</h1>")
+}
 
+func checkAndRender(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
 
 func main() {
 
 	homeView = views.NewView("bootstrap", "views/home.gohtml")
 	contactView = views.NewView("bootstrap", "views/contact.gohtml")
+	faqView = views.NewView("bootstrap", "views/faq.gohtml")
+	signupView = views.NewView("bootstrap", "views/signup.gohtml")
 
 	var notFoundHandle http.Handler = http.HandlerFunc(notFound)
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
+	r.HandleFunc("/faq", faq)
+	r.HandleFunc("/signup", signup)
 	r.NotFoundHandler = notFoundHandle
 
 	http.ListenAndServe(":3000", r)
